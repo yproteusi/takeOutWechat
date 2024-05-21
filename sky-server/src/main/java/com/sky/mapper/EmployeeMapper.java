474e79b357d2fa6d75f6c2c6a0f05b5418521c79
yproteusi <proteusjyl@gmail.com>
2024-05-21 20:41:24 +1200

    add new employee function

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,7 +13,15 @@ public interface EmployeeMapper {
      * @param username
      * @return
      */
-    @Select("select * from employee where username = #{username}")
+    @Select("SELECT  * FROM employee WHERE username = #{username}")
     Employee getByUsername(String username);
 
+    /**
+     * 根据用户名查询员工
+     * @param employee
+     */
+    @Insert("INSERT INTO employee(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user)"
+            +
+            "VALUES(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},now(),now(),#{createUser},#{updateUser})")
+    void insert(Employee employee);
 }
